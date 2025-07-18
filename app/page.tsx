4cb2d96af686b6fced341471d75805f291b58654
Ela <eladebichi@gmail.com>
2025-07-18 12:53:02 +0100

    Add GA

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import { Header } from "@/components/header"
-import { About } from "@/components/about"
-import { Skills } from "@/components/skills"
-import { Resume } from "@/components/resume"
-import { Portfolio } from "@/components/portfolio"
-import { Contact } from "@/components/contact"
-import { Footer } from "@/components/footer"
+import { Header } from "@/components/header";
+import { About } from "@/components/about";
+import { Skills } from "@/components/skills";
+import { Resume } from "@/components/resume";
+import { Portfolio } from "@/components/portfolio";
+import { Contact } from "@/components/contact";
+import { Footer } from "@/components/footer";
+import { GoogleAnalytics } from "@/components/ga";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
+      <GoogleAnalytics />
       <Header />
       <main className="lg:ml-80">
         <About />
@@ -19,5 +21,5 @@ export default function Home() {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
